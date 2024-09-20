7466eb8a154596d0fa9abc6e713af21dc8ec4db2
23DP3DBesp <153283400+23DP3DBesp@users.noreply.github.com>
2024-09-20 06:32:31 +0000

    Grades and Points

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -7,13 +7,31 @@ public class App
     public static void main( String[] args )
     {
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Give speed:");
-        int speed = Integer.valueOf(scanner.nextLine());
-        if (speed >= 120) {
-            System.out.println("Speeding ticket!");
+        System.out.println("Give points [0-100]:");
+        int grade = Integer.valueOf(scanner.nextLine());
+        if (grade < 0) {
+            System.out.println("impossible!");
+        }
+        else if (grade <=49) {
+            System.out.println("Grade: failed");
+        }
+        else if (grade <=59) {
+            System.out.println("Grade: 1");
+        }
+        else if (grade <=69) {
+            System.out.println("Grade: 2");
+        }
+        else if (grade <=79) {
+            System.out.println("Grade: 3");
+        }
+        else if (grade <=89) {
+            System.out.println("Grade: 4");
+        }
+        else if (grade <=100) {
+            System.out.println("Grade: 5");
         }
         else {
-            
+            System.out.println("Grade: incredible!");
         }
     }
 
