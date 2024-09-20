69929052c8d7d0671a3ccf4015539d3eb09aa87f
23DP3DBesp <153283400+23DP3DBesp@users.noreply.github.com>
2024-09-20 05:46:18 +0000

    Speeding ticket

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -6,34 +6,15 @@ public class App
 {
     public static void main( String[] args )
     {
-        // double floating = 0.42;
-        // boolean irTrueOrFalse = true;
-        // Scanner scanner =  new Scanner(System.in);
-        // String valueAsString = scanner.nextLine();
-        // int value = Integer.valueOf(valueAsString);
-        // value = value + 20
-        // System.out.printLn(value);
-        // boolean boolValue = Boolean.valueOf(scanner.nextLine());
-
-        // int calculationWithParentheses = (1+1) + 3 * (2+5);
-        // System.out.println(calculationWithParentheses);
-
-        // int calculationWithoutParentheses = 1+1 + 3 * 2+5;
-        // System.out.println(calculationWithoutParentheses);
-
-        // double first = 3.0;
-        // int second = 2;
-        // System.out.println(first/second); // prints 1.5 (Jā bus first int, tad prints 1)
         Scanner scanner = new Scanner(System.in);
-        int first = Integer.valueOf(scanner.nextLine());
-        int second = Integer.valueOf(scanner.nextLine());
-    
-        System.out.println(first + "+" + second + "=" + (first+second));
-        System.out.println(first + "-" + second + "=" + (first-second));
-        System.out.println(first + "*" + second + "=" + (first*second));
-        System.out.println(first + " un " + second + " vid = " + ((double)(first+second)/2));
-
-
+        System.out.println("Give speed:");
+        int speed = Integer.valueOf(scanner.nextLine());
+        if (speed >= 120) {
+            System.out.println("Speeding ticket!");
+        }
+        else {
+            
+        }
     }
 
 }
