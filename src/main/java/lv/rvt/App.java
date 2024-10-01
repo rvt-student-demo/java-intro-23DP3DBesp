f6124d88b8376a39779e8315d652f11a02d6d383
23DP3DBesp <153283400+23DP3DBesp@users.noreply.github.com>
2024-10-01 19:07:36 +0000

    Counting

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -7,20 +7,9 @@ public class App
     public static void main( String[] args )
     {
         Scanner reader = new Scanner(System.in);
-        int sum = 0;
-        int numbers = 0;
-        while (true) {
-            System.out.println("Give a number: (print 0 to quit)");
-            int number = Integer.valueOf(reader.nextLine());
-            sum = sum + number;
-            numbers = numbers + 1;
-            if (number == 0){
-                System.out.println("Number of numbers: "+ numbers);
-                System.out.println("Sum of numbers: "+ sum);
-                break;
-            }
-            
-
+        int number =  Integer.valueOf(reader.nextLine());
+        for (int i = 0; i < number+1; i++) {
+            System.out.println(i);
         }
     }
 
