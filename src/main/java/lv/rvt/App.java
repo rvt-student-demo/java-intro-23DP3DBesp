daaf1e6784bb367adddf4ae4cc2f27b99d9d0788
23DP3DBesp <153283400+23DP3DBesp@users.noreply.github.com>
2024-10-11 06:20:38 +0000

    Star sign (4 parts)

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -6,19 +6,54 @@ public class App
 {
     public static void main( String[] args )
     {
-    Scanner reader = new Scanner(System.in);
-    int beginning = Integer.valueOf(reader.nextLine());
-    int end = Integer.valueOf(reader.nextLine());
-
-    divisibleByThreeInRange(beginning, end);
+        // printStars(5);
+        // printStars(3);
+        // printStars(9);
+        // printSquare(4);
+        // printRectangle(3, 17);
+        // printTriangle(5);
     }
-    public static void divisibleByThreeInRange(int beginning, int end)
+    public static void printStars(int number)
+    // Part 1
     {
-        for (int i = beginning; i<=end;i++)
+        for (int i = 1; i <= number; i++)
         {
-            if (i % 3 ==0){
-                System.out.println(i);
+            System.out.print("*");
+        }
+        System.out.println("");
+    }
+
+    public static void printSquare(int size)
+    // Part 2
+    {
+        for (int i = 1; i<=size; i++){
+            for (int j = 1; j<=size; j++){
+                System.out.print("*");
             }
+            System.out.println();
         }
     }
+    public static void printRectangle(int wide, int height)
+    // Part 3
+    {
+        for (int i = 1; i<=wide; i++){
+            for (int j = 1; j<=height; j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+    public static void printTriangle(int traingleheight)
+    // Part 4
+    {
+        for (int i = 1; i<= traingleheight; i++)
+        {
+            for (int j = 1; j <= i; j++)
+            {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+    
 }
