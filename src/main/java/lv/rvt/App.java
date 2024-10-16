2310d91a629bb748ab49671e54e2c46ed1186d25
23DP3DBesp <153283400+23DP3DBesp@users.noreply.github.com>
2024-10-16 06:49:33 +0000

    Third element

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -1,89 +1,18 @@
 package lv.rvt;
 
 import java.util.Scanner;
+import java.util.ArrayList;
 
 public class App 
 {
     public static void main( String[] args )
     {
-        // printStars(5);
-        // printStars(3);
-        // printStars(9);
-        // printSquare(4);
-        // printRectangle(3, 17);
-        // printTriangle(4);
-        christmasTree(5);
-    }
-    public static void printStars(int number)
-    // Part 1
-    {
-        for (int i = 1; i <= number; i++)
-        {
-            System.out.print("*");
-        }
-        System.out.println("");
-    }
-
-    public static void printSquare(int size)
-    // Part 2
-    {
-        for (int i = 1; i<=size; i++){
-            for (int j = 1; j<=size; j++){
-                System.out.print("*");
-            }
-            System.out.println();
-        }
-    }
-    public static void printRectangle(int wide, int height)
-    // Part 3
-    {
-        for (int i = 1; i<=wide; i++){
-            for (int j = 1; j<=height; j++){
-                System.out.print("*");
-            }
-            System.out.println();
-        }
-    }
-    public static void printTriangle(int traingleheight)
-    // Part 4
-    {
-        for (int i = 1; i<= traingleheight; i++)
-        {
-            for (int j = 1; j <= i; j++)
-            {
-                System.out.print("*");
-            }
-            System.out.println();
-        }
-        
-    }
-    public static void printSpaces(int number)
-    {
-        for (int i = 0; i <= number; i++)
-        {
-            System.out.print(" ");
-        }
-    }
-    public static void printRightTriangle(int size)
-    {
-        for (int i = 0; i <= size; i++)
-        {
-            printSpaces(size-i);
-            printStars(i);
-        }
-    }
-    public static void christmasTree(int height)
-    {
-        for (int i = 0; i <= height; i++)
-        {
-            printSpaces(height-1);
-            printStars(i+(i-1));
-        }
-        for (int i = 0; i < 2; i ++)
-        {
-            printSpaces(height-2);
-            printStars(3);
-        }
+        ArrayList<String> wordlist = new ArrayList<>();
+        wordlist.add("Tom");
+        wordlist.add("Emma");
+        wordlist.add("Alex");
+        wordlist.add("Mary");
+        System.out.println(wordlist.get(2));
     }
     
 }
