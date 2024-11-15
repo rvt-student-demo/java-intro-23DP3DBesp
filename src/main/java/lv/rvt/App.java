0b269f4f40749d2ba5d8778c0079d24b89296d0b
23DP3DBesp <153283400+23DP3DBesp@users.noreply.github.com>
2024-11-15 07:00:07 +0000

    DecreasingCounter

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -1,20 +1,21 @@
 package lv.rvt;
 
-import java.util.Scanner;
-
 public class App 
 {
-    public static void main( String[] args )
+
+public static void main( String[] args )
 
     {
-        Account denissAccount = new Account("Deniss account", 100.00);
-        
-        System.out.println("Initial state");
-        System.out.println(denissAccount);
-        
-        denissAccount.deposit(20);
-        System.out.println("The balance of Mikhail's account is now: " + denissAccount.balance());
-        
-        System.out.println("End state");
+        DecreasingCounter counter = new DecreasingCounter(100);
+
+        counter.printValue();
+
+        counter.reset();
+        counter.printValue();
+
+        counter.decrement();
+        counter.printValue();
+
+
     }
 }
---
+++ src/main/java/lv/rvt/DecreasingCounter.java
@@ -0,0 +1,26 @@
+package lv.rvt;
+    public class DecreasingCounter {
+        private int value;   // a variable that remembers the value of the counter
+
+        public DecreasingCounter(int initialValue) {
+            this.value = initialValue;
+        }
+
+        public void printValue() {
+            System.out.println("value: " + this.value);
+        }
+
+        public void decrement() {
+            if (this.value > 0){
+            this.value -= 1;
+            }
+            // write the method implementation here
+            // the aim is to decrement the value of the counter by one
+        }
+
+        public void reset(){
+            this.value = 0;
+        }
+
+        // and the other methods go here
+    }
